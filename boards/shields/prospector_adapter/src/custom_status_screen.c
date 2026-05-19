cacd85ef826fbe2d91a1be515b5992c472babe25
hrmt-lab <hrmt0805@gmail.com>
2026-05-19 16:09:50 +0900

    Keep only ring status screen layout

--- boards/shields/prospector_adapter/src/custom_status_screen.c
+++ boards/shields/prospector_adapter/src/custom_status_screen.c
@@ -1,15 +1,3 @@
 #include <lvgl.h>
 
-#if defined(CONFIG_PROSPECTOR_STATUS_SCREEN_CLASSIC)
-#include "layouts/classic/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_RADII)
-#include "layouts/radii/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_FIELD)
-#include "layouts/field/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_OPERATOR)
-#include "layouts/operator/status_screen.c"
-#elif defined(CONFIG_PROSPECTOR_STATUS_SCREEN_RING)
 #include "layouts/ring/status_screen.c"
-#else
-#error "No status screen layout selected"
-#endif
